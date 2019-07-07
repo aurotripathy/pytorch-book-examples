22d0ad66b474f4c5149ae1aff19905cd9102e975
Auro Tripathy <ipserv@yahoo.com>
2019-07-07 11:35:27 -0700

    added misclassified

--- rbfn/utils.py
+++ rbfn/utils.py
@@ -1,5 +1,8 @@
 import torch.nn as nn
-import numpy as np
+from pudb import set_trace
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import os
 
 def print_network(net):
     num_params = 0
@@ -22,66 +25,57 @@ def initialize_weights(net):
             m.bias.data.zero_()
 
 
+def display(Xs, ys, title):
+    category_1_x1 = []
+    category_1_x2 = []
+    category_2_x1 = []
+    category_2_x2 = []
 
-def kmeans(X, k):
-    """Performs k-means clustering for 1D input
-    
-    Arguments:
-        X {ndarray} -- A Mx1 array of inputs
-        k {int} -- Number of clusters
-    
-    Returns:
-        ndarray -- A kx1 array of final cluster centers
-    """
- 
-    # randomly select initial clusters from input data
-    clusters = np.random.choice(np.squeeze(X), size=k)
-    prev_clusters = clusters.copy()
-    stds = np.zeros(k)
-    converged = False
- 
-    while not converged:
-        """
-        compute distances for each cluster center to each point 
-        where (distances[i, j] represents the distance between the ith point and jth cluster)
-        """
-        distances = np.squeeze(np.abs(X[:, np.newaxis] - clusters[np.newaxis, :]))
- 
-        # find the cluster that's closest to each point
-        closestCluster = np.argmin(distances, axis=1)
- 
-        # update clusters by taking the mean of all of the points assigned to that cluster
-        for i in range(k):
-            pointsForCluster = X[closestCluster == i]
-            if len(pointsForCluster) > 0:
-                clusters[i] = np.mean(pointsForCluster, axis=0)
- 
-        # converge if clusters haven't moved
-        converged = np.linalg.norm(clusters - prev_clusters) < 1e-6
-        prev_clusters = clusters.copy()
- 
-    distances = np.squeeze(np.abs(X[:, np.newaxis] - clusters[np.newaxis, :]))
-    closestCluster = np.argmin(distances, axis=1)
- 
-    clustersWithNoPoints = []
-    for i in range(k):
-        pointsForCluster = X[closestCluster == i]
-        if len(pointsForCluster) < 2:
-            # keep track of clusters with no points or 1 point
-            clustersWithNoPoints.append(i)
-            continue
+    for X, y in zip(Xs, ys):
+        if y == 0:
+            category_1_x1.append(X[0])
+            category_1_x2.append(X[1])
         else:
-            stds[i] = np.std(X[closestCluster == i])
- 
-    # if there are clusters with 0 or 1 points, take the mean std of the other clusters
-    if len(clustersWithNoPoints) > 0:
-        pointsToAverage = []
-        for i in range(k):
-            if i not in clustersWithNoPoints:
-                pointsToAverage.append(X[closestCluster == i])
-        pointsToAverage = np.concatenate(pointsToAverage).ravel()
-        stds[clustersWithNoPoints] = np.mean(np.std(pointsToAverage))
- 
-    return clusters, stds
+            category_2_x1.append(X[0])
+            category_2_x2.append(X[1])
+
+    plt.clf()
+    plt.scatter(category_1_x1, category_1_x2, marker='^', c='lightgreen', label='cat_1')
+    plt.scatter(category_2_x1, category_2_x2, marker='o', c='lightblue', label='cat_2')
+
+    plt.title(title)
+    plt.legend(loc='upper right')
+    # plt.show()
+    plt.savefig(os.path.join('io', title))
 
 
+def display_misclassified(Xs, ys_gt, ys_preds, title):
+    category_1_x1 = []
+    category_1_x2 = []
+    category_2_x1 = []
+    category_2_x2 = []
+    category_miss_class_x1 = []
+    category_miss_class_x2 = []
+    
+    for X, y_gt, y_pred in zip(Xs, ys_gt, ys_preds):
+        if y_gt == 0:
+            category_1_x1.append(X[0])
+            category_1_x2.append(X[1])
+        else:
+            category_2_x1.append(X[0])
+            category_2_x2.append(X[1])
+        if y_gt != y_pred:
+            category_miss_class_x1.append(X[0])
+            category_miss_class_x2.append(X[1])
+
+    plt.clf()
+    plt.scatter(category_1_x1, category_1_x2, marker='^', c='lightgreen', label='cat_1')
+    plt.scatter(category_2_x1, category_2_x2, marker='o', c='lightblue', label='cat_2')
+    plt.scatter(category_miss_class_x1, category_miss_class_x2,
+                s=80, facecolors='none', edgecolors='r', label='miss')
+
+
+    plt.title(title)
+    plt.legend(loc='upper right')
+    # plt.show()
+    plt.savefig(os.path.join('io', title))
