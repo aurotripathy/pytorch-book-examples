c670ce08026ec1a98773f078eeaa68e7069fc5b4
Auro Tripathy <ipserv@yahoo.com>
2019-07-17 21:41:14 -0700

    added epoch loss

--- slp/single-layer-binary-classifier.py
+++ slp/single-layer-binary-classifier.py
@@ -8,6 +8,7 @@ import torch.optim as optimizer
 
 
 class LinearClassifier(nn.Module):
+    """ Linear layer followed by Sigmoid non-linearity"""
     def __init__(self):
         super(LinearClassifier, self).__init__()
         self.fully_connected = Linear(2, 1)
@@ -18,38 +19,39 @@ class LinearClassifier(nn.Module):
         return self.sigmoid(self.fully_connected(x))
 
 
-linear_model = LinearClassifier()
+model = LinearClassifier()
 criterion = nn.MSELoss()
-optimizer = optimizer.SGD(linear_model.parameters(), lr=0.01, momentum=0.5)
+optimizer = optimizer.SGD(model.parameters(), lr=0.01, momentum=0.5)
 
 train_set = [((-2, -1), 0), ((-2, 1), 1), ((-1, -1.5), 0),
              ((1, 1), 1), ((1.5, -0.5), 1), ((2, -2), 0)]
 
-linear_model.train()
+model.train()
 for epoch in range(1000):
+    epoch_loss = 0
     for train_data in train_set:
         X = torch.tensor([train_data[0]], dtype=torch.float, requires_grad=True)
         y = torch.tensor([train_data[1]], dtype=torch.float, requires_grad=True)
-                                       
         optimizer.zero_grad()
-        y_pred = linear_model(X)
-        loss = criterion(y_pred, y)
+        y_pred = model(X)
+        loss = criterion(torch.squeeze(y_pred, 1), y)
+        epoch_loss += loss
         loss.backward()
         optimizer.step()
-        print('Epoch {}, loss:{}'.format(epoch, loss))
+    print('Epoch {}, Epoch loss:{}'.format(epoch, epoch_loss))
 
-print('Model params:', list(linear_model.parameters()))  # https://graphsketch.com/
+print('Model params:', list(model.parameters()))  # https://graphsketch.com/
 
 # Test on unseen data
 test_set = [(0.5, 0.5), (1.5, 1.5), (2, -1), (-3, -3)]
-linear_model.eval()
+model.eval()
 for test_data in test_set:
-    out = linear_model(torch.tensor([test_data], dtype=torch.float, requires_grad=False))
-    print('Data in: {}, out prob {}, class {}'.format(test_data, out, '0' if out < 0.5 else '1'))
+    out = model(torch.tensor([test_data], dtype=torch.float, requires_grad=False))
+    print('Data in: {}, Out prob: {}, class {}'.format(test_data, out, '0' if out < 0.5 else '1'))
 
 # Test on training data
 for train_data in train_set:
-    prob = linear_model(torch.tensor([train_data[0]], dtype=torch.float, requires_grad=False))
+    prob = model(torch.tensor([train_data[0]], dtype=torch.float, requires_grad=False))
     label = 0 if prob < 0.5 else 1
     verdict = 'correct' if label == train_data[1] else 'wrong'
-    print('Data in: {}, out prob {}, class {}: {}'.format(train_data, prob, label, verdict))
+    print('Data in: {}, Out prob: {}, class {}: {}'.format(train_data, prob, label, verdict))
