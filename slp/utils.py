eb836c24790f2866eca5702bbdad0690f58ecc29
Auro Tripathy <ipserv@yahoo.com>
2019-09-02 16:31:46 -0700

    initial

---
+++ slp/utils.py
@@ -0,0 +1,94 @@
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import ylim, xlim, axhline, axvline
+
+import numpy as np
+import os
+
+def display_loss(loss, title):
+
+    x_axis = np.arange(len(loss))
+    y_axis = loss
+
+    fig, ax = plt.subplots()
+    ax.plot(x_axis, y_axis)
+
+    ax.set(xlabel='Epochs', ylabel='Epoch Loss',
+           title=title)
+    ax.grid()
+
+    fig.savefig("loss.png")
+    plt.show()
+
+
+
+def display_points(Xs, ys, title):
+    category_1_x1 = []
+    category_1_x2 = []
+    category_2_x1 = []
+    category_2_x2 = []
+
+    for X, y in zip(Xs, ys):
+        if y == 0:
+            category_1_x1.append(X[0])
+            category_1_x2.append(X[1])
+        else:
+            category_2_x1.append(X[0])
+            category_2_x2.append(X[1])
+
+    plt.clf()
+    ylim((-3, 3))
+    xlim((-3, 3))
+    plt.grid(True)
+    axhline(linewidth=2, color='k') #adds thick black line @ y=0
+    axvline(linewidth=2, color='k') #adds thick black line @ x=0
+    plt.scatter(category_1_x1, category_1_x2, marker='o', c='red', label='cat_1')
+    plt.scatter(category_2_x1, category_2_x2, marker='s', c='green', label='cat_2')
+
+    plt.title(title)
+    plt.legend(loc='upper right')
+    # plt.show()
+    plt.savefig(os.path.join('io', title))
+
+def plot_points_line_slope_intercept(Xs, ys, slope, intercept, title,
+                                     x_range=(-3, 3)):
+    """Plot points and line from slope and intercept"""
+
+    category_1_x1 = []
+    category_1_x2 = []
+    category_2_x1 = []
+    category_2_x2 = []
+
+    for X, y in zip(Xs, ys):
+        if y == 0:
+            category_1_x1.append(X[0])
+            category_1_x2.append(X[1])
+        else:
+            category_2_x1.append(X[0])
+            category_2_x2.append(X[1])
+
+    plt.clf()
+    ylim((-3, 3))
+    xlim((-3, 3))
+    plt.grid(True)
+    axhline(linewidth=2, color='k') #adds thick black line @ y=0
+    axvline(linewidth=2, color='k') #adds thick black line @ x=0
+    plt.scatter(category_1_x1, category_1_x2, marker='o', c='red', label='cat_1')
+    plt.scatter(category_2_x1, category_2_x2, marker='s', c='green', label='cat_2')
+
+    plt.title(title)
+    plt.legend(loc='upper right')
+
+
+    x_vals = np.arange(x_range[0], x_range[1] + 1, 1)
+    y_vals = intercept + slope * x_vals
+    plt.grid(True)
+    axhline(linewidth=2, color='k') #adds thick black line @ y=0
+    axvline(linewidth=2, color='k') #adds thick black line @ x=0
+    ylim((x_range[0], x_range[1]))
+    xlim((x_range[0], x_range[1]))
+    plt.plot(x_vals, y_vals, '-')
+    plt.show()
+    plt.close()
+    
+if __name__ == '__main__':
+    plot_line_from_slope_intercept(0.5,  0)
