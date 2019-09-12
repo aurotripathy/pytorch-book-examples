17883f839f4e970d4410c8183f28a1c063e718f3
Auro Tripathy <ipserv@yahoo.com>
2019-09-11 21:46:02 -0700

    cleanup

--- slp/linear-classifier-pytorch-gpu.py
+++ slp/linear-classifier-pytorch-gpu.py
@@ -58,9 +58,10 @@ test_set = [(0.5, 0.5), (1.5, 1.5), (2, -1), (-3, -3)]
 model.eval()
 for test_data in test_set:
     out = model(torch.tensor([test_data], dtype=torch.float, requires_grad=False))
-    print('Data in: {}, Out Prob: {}, Predicted Class {}'.format(test_data,
-                                                                 out,
-                                                                 '0' if out < 0.5 else '1'))
+    pred_class = '0' if out < 0.5 else '1'
+    print('Data in:{}, Out Prob:{}, Predicted Class:{}'.format(test_data,
+                                                               out,
+                                                               pred_class))
 
 (w1, w2) = model.cpu().fully_connected.weight.data.numpy()[0]
 b = model.fully_connected.bias.data.numpy()[0]
