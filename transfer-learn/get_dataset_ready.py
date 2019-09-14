4cf9077223525655fe3591689e6c63ab6032e082
Auro Tripathy <ipserv@yahoo.com>
2019-09-14 12:33:45 -0700

    wip

--- transfer-learn/get_dataset_ready.py
+++ transfer-learn/get_dataset_ready.py
@@ -3,6 +3,9 @@ import os
 import glob
 from sklearn.model_selection import train_test_split
 import random
+from shutil import copyfile
+import sys
+from sys import exit
 
 def extract_frames(movie_file):
     folder = movie_file.split('/')[-1].split('.')[0]
@@ -31,8 +34,24 @@ def extract_frames(movie_file):
             break
 
         count += 1
-    print(count)
+    print('Images count', count)
     return dir_path
+
+def copy_files(source_filenames, dest_path):
+    for source_file in source_filenames:
+        dest_file = os.path.join(dest_path, os.path.basename(source_file))
+
+        print(source_file, dest_file)
+        
+        try:
+            copyfile(source_file, dest_file)
+        except IOError as e:
+            print("Unable to copy file. %s" % e)
+            exit(1)
+        except:
+            print("Unexpected error:", sys.exc_info())
+            exit(1)
+    
 
 def create_train_val_test(all_folder):
     image_files = []
@@ -43,17 +62,27 @@ def create_train_val_test(all_folder):
     split_1 = int(0.8 * len(image_files))
     split_2 = int(0.9 * len(image_files))
     train_filenames = image_files[:split_1]
-    print(train_filenames)
+    print('Total train files', len(train_filenames))
     val_filenames = image_files[split_1:split_2]
-    print(val_filenames)
+    print('Total val files', len(val_filenames))
     test_filenames = image_files[split_2:]
     
     train_path = os.path.join(os.path.dirname(all_folder), 'train')
     val_path = os.path.join(os.path.dirname(all_folder), 'val')
     print(train_path)
     print(val_path)
+
+    if not os.path.exists(train_path):
+        os.makedirs(train_path)
+    copy_files(train_filenames, train_path)
+
+    if not os.path.exists(val_path):
+        os.makedirs(val_path)
+    copy_files(val_filenames, val_path)
+
     
 dir_path = extract_frames('videos/batman.MOV')
 create_train_val_test(dir_path)
 dir_path = extract_frames('videos/superman.MOV')
+create_train_val_test(dir_path)
 
