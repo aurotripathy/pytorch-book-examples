9f8c0ca9f322ff83e9fbb11dc1e200e4bca80021
Auro Tripathy <ipserv@yahoo.com>
2019-09-19 23:28:25 -0700

    added test folder

--- transfer-learn/utils.py
+++ transfer-learn/utils.py
@@ -35,14 +35,22 @@ def load_data(data_dir):
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
         ]),
+        'test': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+        ]),
     }
 
     image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
                                               data_transforms[x])
-                      for x in ['train', 'val']}
+                      for x in ['train', 'val', 'test']}
     dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
                                                   shuffle=True, num_workers=4)
-                   for x in ['train', 'val']}
+                   for x in ['train', 'val', 'test']}
     dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
     class_names = image_datasets['train'].classes
     return dataloaders, dataset_sizes, class_names
+
