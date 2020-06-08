f6752a4999808302a4609be8093888ac26a8b94c
Auro Tripathy <ipserv@yahoo.com>
2020-06-07 17:09:49 -0700

    cleanup

--- attention/pyt_model.py
+++ attention/pyt_model.py
@@ -24,25 +24,11 @@ class Attn(torch.nn.Module):
         self.human_vocab_size = human_vocab_size
         self.machine_vocab_size = machine_vocab_size
         self.batch_size = batch_size
-        ## TF # self.repeator = RepeatVector(Tx)  # Repeats the input Tx times.
-        # self.repeator = torch.repeat(Tx)
-        
-        ## TF # self.concatenator = Concatenate(axis=-1)
-        # self.concatenator = torch.cat(axis=-1)  # not needed
-        
-        ## TF # self.densor1 = Dense(10, activation = "tanh", name='Dense1')
+
         self.densor1 = nn.Linear(128, 10)  # TBD input?? 
 
-        ## TF # self.densor2 = Dense(1, activation = "relu", name='Dense2')
         self.densor2 = nn.Linear(10, 1)
         
-        # We are using a custom softmax(axis = 1) loaded in this notebook
-        ## TF # self.activator = Activation(softmax, name='attention_weights')
-        self.activator = SoftMax()
-
-
-        ## TF # self.dotor = Dot(axes = 1)
-        # self.dotor = torch.mm(axes = 1)
 
         self.bi_d_lstm = nn.LSTM(self.human_vocab_size, self.n_a, 1,  # in size, out size, num layers
                                  batch_first=False, bidirectional=True)
@@ -56,6 +42,7 @@ class Attn(torch.nn.Module):
         self.state = torch.zeros(1, self.batch_size, self.n_state)
         self.context = torch.zeros(1,self.batch_size, self.n_state,)
 
+        
     def _one_step_attention(self, a, s_prev):
         """
         Performs one step of attention: Outputs a context vector computed as a dot product of the attention weights
@@ -69,13 +56,10 @@ class Attn(torch.nn.Module):
         context -- context vector, input of the next (post-attetion) LSTM cell
         """
 
-        # Use repeator to repeat s_prev to be of shape (m, Tx, n_s) so that you can concatenate it with all hidden states "a" (≈ 1 line)
-        # s_prev = self.repeator(s_prev)
         print('s_prev shape', s_prev.shape)
         s_prev = s_prev.repeat(self.Tx, 1,1)
         print('s_prev shape', s_prev.shape)
         
-        # Use concatenator to concatenate a and s_prev on the last axis (≈ 1 line)
         concat = torch.cat((a, s_prev), dim=-1)
         print('concat shape', concat.shape)
 
@@ -85,16 +69,14 @@ class Attn(torch.nn.Module):
 
         # Use densor2 to propagate e through a small fully-connected neural network to compute the "energies" variable energies. (≈1 lines)
         energies = F.relu(self.densor2(e))
+
         # Use "activator" on "energies" to compute the attention weights "alphas" (≈ 1 line)
-        # alphas = self.activator(energies)
         alphas = F.softmax(energies)
         print('alphas shape', alphas.shape)
         print('a shape', a.size())
-        # Use dotor together with "alphas" and "a" to compute the context vector to be given to the next (post-attention) LSTM-cell (≈ 1 line)
-        # context = self.dotor([alphas, a])
-        context =  torch.matmul(alphas.transpose(1,0).transpose(2,1), a.transpose(1,0))
-        # context = torch.mm(alphas, a)
 
+        # Use dotor together with "alphas" and "a" to compute the context vector to be given to the next (post-attention) LSTM-cell (≈ 1 line)
+        context =  torch.matmul(alphas.transpose(1,0).transpose(2,1), a.transpose(1,0))
 
         return context
 
@@ -102,32 +84,29 @@ class Attn(torch.nn.Module):
     def forward(self, x):
         """
         Arguments:
-        X -- input
+        x -- input
         Returns:
         model -- Pytorch model instance
         """
         # Initialize empty list of outputs
         outputs = []
 
-        # Step 1: Define your pre-attention Bi-LSTM. Remember to use return_sequences=True. (≈ 1 line)
-        ## TF # a = Bidirectional(LSTM(self.n_a, return_sequences=True, name='bidirectional_1'), merge_mode='concat')(X)
-        # Bi-LSTM input is of shape (seq_len, batch, input_size) 30, 100, 37
+        # Step 1: Define your pre-attention Bi-LSTM. 
         a, _ = self.bi_d_lstm(x)
 
         # Step 2: Iterate for Ty steps
         for t in range(self.Ty):
 
-            # Step 2.A: Perform one step of the attention mechanism to get back the context vector at step t (≈ 1 line)
+            # Step 2.A: Perform one step of the attention mechanism to get back the context vector at step t
             attn_context = self._one_step_attention(a, self.state)
 
             # Step 2.B: Apply the post-attention LSTM cell to the "context" vector.
             # Don't forget to pass: initial_state = [hidden state, cell state] (≈ 1 line)
-            # set_trace()
             _, (self.state, self.context) = self.post_activation_LSTM_cell(attn_context, (self.state, self.context))
 
             # Step 2.C: Apply Dense layer to the hidden state output of the post-attention LSTM (≈ 1 line)
             out = F.softmax(self.output_layer(self.state))
-            print('out shape', out.size())
+            print('output_layer shape', out.size())
 
             # Step 2.D: Append "out" to the "outputs" list (≈ 1 line)
             outputs.append(out)
