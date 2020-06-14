acc919dbc58ad3a7f9a04d1504cc5f012617c8f6
Auro Tripathy <ipserv@yahoo.com>
2020-06-13 18:12:16 -0700

    cleanup for debug

--- attention/neural_machine_translation_attention.py
+++ attention/neural_machine_translation_attention.py
@@ -59,7 +59,7 @@ print("Machine vocab", machine_vocab)
 print("Length Human vocab", len(human_vocab))
 print("Length Machine vocab", len(machine_vocab))
 
-
+set_trace()
 Xoh = torch.from_numpy(Xoh).float()
 Yoh = torch.from_numpy(Yoh).float()
 
@@ -68,7 +68,6 @@ Yoh = np.transpose(Yoh, (1, 0, 2))
 print("Tensor Xoh.shape:", Xoh.size())
 print("Numpy  Yoh.shape:", Yoh.size())
 
-device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 epochs = 50
 batch_size = 100
 n_batches = Xoh.size()[1] // batch_size
@@ -77,22 +76,16 @@ n_a = 32  # hidden state size of the Bi-LSTM
 n_s = 64  # hidden state size of the post-attention LSTM
 model = Attn(Tx, Ty, n_a, n_s, len(human_vocab), len(machine_vocab), batch_size)
 
-outputs = model(Xoh[:, 0:batch_size, :])  # Dim = 30 x 100 x 37
-
 optimizer = optim.Adam(model.parameters(),
                        lr=0.005, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.001)
 criterion = nn.CrossEntropyLoss()
 
-s0 = np.zeros((m, n_s))
-c0 = np.zeros((m, n_s))
-
-# scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
 for epoch in range(1, epochs + 1):
-    # TODO remember to shuffle data
+    # TODO shuffle data
     model.train()  # Turn on the train mode
     for i in range(n_batches):
         local_Xoh, local_Yoh = Xoh[:, i*n_batches:(i+1)*n_batches,], Yoh[:, i*n_batches:(i+1)*n_batches,]
-        # local_Xoh, local_Yoh = local_Xoh.to(device), local_Yoh.to(device)
+
         optimizer.zero_grad()
         outputs = model(local_Xoh).transpose(1,0).transpose(2,1)
         print('shape of outputs', outputs.size())
@@ -106,21 +99,3 @@ for epoch in range(1, epochs + 1):
 
 
 torch.save(model.state_dict(), 'models/attn-model.pt')
-
-# Inference
-model.load_weights('models/attn-model.h5')
-
-EXAMPLES = ['Friday 3 May 1979', '5 April 09', '21th of August 2016', 'Tue 10 Jul 2007', 'Saturday May 9 2018', 'March 3 2001', 'March 3rd 2001', '1 March 2001']
-for example in EXAMPLES:
-    
-    source = string_to_int(example, Tx, human_vocab)
-    source = np.array(list(map(lambda x: to_categorical(x, num_classes=len(human_vocab)), source))).swapaxes(0,1)
-    source = np.expand_dims(np.transpose(source), axis=0)
-    prediction = model.predict([source, s0, c0])
-    prediction = np.argmax(prediction, axis = -1)
-    output = [inv_machine_vocab[int(i)] for i in prediction]
-    
-    print("source:", example)
-    print("output:", ''.join(output))
-
-
