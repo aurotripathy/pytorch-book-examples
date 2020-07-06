92a04892995498f7aead047023a57422ca760e55
Auro Tripathy <ipserv@yahoo.com>
2020-07-05 22:15:17 -0700

    cleanup

--- new_attn/attn2.py
+++ new_attn/attn2.py
@@ -7,8 +7,8 @@ import numpy as np
 import random
 from pudb import set_trace
 
-Tx = 30  # human time-steps 30
-Ty = 10  # machine time-steps 10
+seq_len_human = 30  # human time-steps 30
+seq_len_machine = 10  # machine time-steps 10
 EMBEDDING_DIM_PRE_ATTN = 50
 HIDDEN_DIM_PRE_ATTN_LSTM = 32  # hidden size of pre-attention Bi-LSTM; output is twice of this
 HIDDEN_DIM_POST_ATTN_LSTM = 64
@@ -41,27 +41,25 @@ class EncoderRNN(nn.Module):
 
 class AttnDecoderRNN(nn.Module):
     def __init__(self, hidden_size, output_size,
-                 max_length=Ty):
+                 max_length=seq_len_machine):
         super(AttnDecoderRNN, self).__init__()
         self.hidden_size = hidden_size
         self.output_size = output_size
         self.max_length = max_length
 
-        # self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
-        self.attn = nn.Linear(3840, 30)
-        # self.lstm = nn.LSTM(self.hidden_size * 2, self.hidden_size)
-        self.gru = nn.GRU(self.hidden_size, self.hidden_size)
+        self.attn_wieghted = nn.Linear(3840, 30)
+        self.gru = nn.GRU(self.hidden_size, self.hidden_size)  # TODO convert to LSTM
         self.out = nn.Linear(self.hidden_size, self.output_size)
 
     def forward(self, input, hidden, time_step):
         cat_input_hidden = torch.stack([torch.cat((input[i], hidden[0]), 1) for i in range(30)])
-        attn_weights = F.softmax(self.attn(cat_input_hidden.view(1, 1, -1)), dim=1)
+        attn_weights = F.softmax(self.attn_wieghted(cat_input_hidden.view(1, 1, -1)), dim=1)
         attn_applied = torch.bmm(attn_weights, input.view(1, 30, -1))
 
         output, hidden = self.gru(attn_applied, hidden)
 
         output = F.log_softmax(self.out(output[0]), dim=1)
-        return output, hidden
+        return output, hidden  # TODO n three parameters?
  
     def init_hidden(self):
         return torch.zeros(1, 1, self.hidden_size, device=device)
@@ -76,10 +74,10 @@ print('Machine vocab', machine_vocab)
 print('Inverse machine vocav', inv_machine_vocab)
 
 X, Y = zip(*dataset)
-X, Y, _, _ = preprocess_data(dataset, human_vocab, machine_vocab, Tx, Ty)
+X, Y, _, _ = preprocess_data(dataset, human_vocab, machine_vocab, seq_len_human, seq_len_machine)
 
 
-def evaluate(input_tensor, encoder_rnn, attn_decoder_rnn, target_length=Ty):
+def evaluate(input_tensor, encoder_rnn, attn_decoder_rnn, target_length=seq_len_machine):
 
     with torch.no_grad():
         encoder_outputs = encoder_rnn(input_tensor)
@@ -97,7 +95,7 @@ def evaluate(input_tensor, encoder_rnn, attn_decoder_rnn, target_length=Ty):
 def train(input_tensor, target_tensor,
           encoder_rnn, attn_decoder_rnn,
           encoder_optimizer, attn_decoder_optimizer,
-          criterion, target_length=Ty):
+          criterion, target_length=seq_len_machine):
 
     encoder_optimizer.zero_grad()
     attn_decoder_optimizer.zero_grad()
@@ -127,21 +125,10 @@ attn_decoder_rnn = AttnDecoderRNN(HIDDEN_DIM_POST_ATTN_LSTM, len(machine_vocab))
 
 X = torch.from_numpy(X).long().to(device)
 Y = torch.from_numpy(Y).long().to(device)
+#  TODO split train/test
 
-eval = True
-if eval:
-    print('loading models...')
-    encoder_rnn.load_state_dict(torch.load('encoder_rnn_state.dict'))
-    attn_decoder_rnn.load_state_dict(torch.load('attn_decoder_rnn_state.dict'))
-    
-    for _ in range(100):
-        i = random.choice(range(nb_samples))
-        machine_date = evaluate(X[i], encoder_rnn, attn_decoder_rnn)
-        print('Input Human Date:', dataset[i][0])
-        print('Predicted Machine Date:', machine_date,
-              'Actual Machine Date:', dataset[i][1],
-              'matches' if machine_date == dataset[i][1] else 'mismatch')
-else:  # train
+train = False
+if train:
     encoder_optimizer = optim.SGD(encoder_rnn.parameters(), lr=LEARNING_RATE)
     decoder_optimizer = optim.SGD(attn_decoder_rnn.parameters(), lr=LEARNING_RATE)
     criterion = nn.NLLLoss()
@@ -153,5 +140,17 @@ else:  # train
             if i % 1000 == 0:
                 print(i, total_loss/1000)
                 total_loss = 0
-        torch.save(encoder_rnn.state_dict(), 'encoder_rnn_state.pt')
-        torch.save(attn_decoder_rnn.state_dict(), 'attn_decoder_rnn_state.pt')
+        torch.save(encoder_rnn.state_dict(), 'encoder_rnn_state.dict')
+        torch.save(attn_decoder_rnn.state_dict(), 'attn_decoder_rnn_state.dict')
+else:  # evaluate
+    print('loading models...')
+    encoder_rnn.load_state_dict(torch.load('encoder_rnn_state.dict'))
+    attn_decoder_rnn.load_state_dict(torch.load('attn_decoder_rnn_state.dict'))
+    
+    for _ in range(100):
+        i = random.choice(range(nb_samples))
+        machine_date = evaluate(X[i], encoder_rnn, attn_decoder_rnn)
+        print('Input Human Date:', dataset[i][0])
+        print('Predicted Machine Date:', machine_date,
+              'Actual Machine Date:', dataset[i][1],
+              'matches' if machine_date == dataset[i][1] else 'mismatch')
