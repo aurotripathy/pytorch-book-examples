42fd14b2d628ceef95b00e5ce785ebd45c851d80
Auro Tripathy <ipserv@yahoo.com>
2020-09-12 13:48:44 -0700

    added rgn reinit

--- custom-layers/custom-linear-layer.py
+++ custom-layers/custom-linear-layer.py
@@ -9,10 +9,8 @@ class MyLinearLayer(nn.Module):
     def __init__(self, size_in, size_out):
         super().__init__()
         self.size_in, self.size_out = size_in, size_out
-        weights = torch.Tensor(size_out, size_in)
-        self.weights = nn.Parameter(weights)  # nn.Parameter is a Tensor that's a module parameter.
-        bias = torch.Tensor(size_out)
-        self.bias = nn.Parameter(bias)
+        self.weights = nn.Parameter(torch.Tensor(size_out, size_in))  # nn.Parameter is a Tensor that's a module parameter.
+        self.bias = nn.Parameter(torch.Tensor(size_out))
 
         # initialize weights and biases
         nn.init.kaiming_uniform_(self.weights, a=math.sqrt(5)) # weight init
@@ -26,11 +24,15 @@ class MyLinearLayer(nn.Module):
 
 
 class BasicModel(nn.Module):
-    def __init__(self):
+    def __init__(self, mode):
         super().__init__()
         self.conv = nn.Conv2d(1, 128, 3)
-        # self.linear = nn.Linear(256, 2)
-        self.linear = MyLinearLayer(256, 2)
+        if mode == 'use-library':
+            self.linear = nn.Linear(256, 2)
+        elif mode == 'use-custom':
+            self.linear = MyLinearLayer(256, 2)
+        else:
+            raise Exception('Sorry, must be either use-library or use-custom')
 
     def forward(self, x):
         x = self. conv(x)
@@ -38,9 +40,14 @@ class BasicModel(nn.Module):
         return self.linear(x)
 
 torch.manual_seed(0)  #  for repeatable results
-basic_model = BasicModel()
-inp = np.array([[[[1,2,3,4],  # batch(=1) x channels(=1) x height x width
-                  [1,2,3,4],
-                  [1,2,3,4]]]])
+inp = np.array([[[[1, 2, 3, 4],  # batch(=1) x channels(=1) x height x width
+                  [1, 2, 3, 4],
+                  [1, 2, 3, 4]]]])
 x = torch.tensor(inp, dtype=torch.float)
-print('Forward computation thru model:', basic_model(x))
+
+basic_model = BasicModel('use-library')
+print('Forward computation by using torch library:', basic_model(x))
+
+torch.manual_seed(0)  #  important to reset 
+basic_model = BasicModel('use-custom')
+print('Forward computation by using custom layer:', basic_model(x))
