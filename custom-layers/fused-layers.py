9fd7d905f832a04f511fe7e64a86c182db8007a2
Auro Tripathy <ipserv@yahoo.com>
2020-09-13 14:30:12 -0700

    final

--- custom-layers/fused-layers.py
+++ custom-layers/fused-layers.py
@@ -1,19 +1,5 @@
 import torch
 import torch.nn as nn
-import numpy as np
-import math
-
-def swish(x):
-    """ Swish activation works better than ReLU on deeper models across a number of challenging data sets. """
-    return x * torch.sigmoid(x)
-
-def gelu(x):
-    """ Gaussian Error Linear Unit, defined as the input (x) times 
-        standard Gaussion cumulative dustribution function, cfd. """
-    pi = 3.1415926535897932
-    cdf = 0.5 * (1.0 + torch.tanh((math.sqrt(2 / pi) * (x + 0.044715 * torch.pow(x, 3)))))
-    return x*cdf
-
 
 class LinearPlusActivation(torch.nn.Linear):
     """ Fused Linear and activation Module. """
@@ -29,22 +15,18 @@ class LinearPlusActivation(torch.nn.Linear):
         return self.activation_func(nn.functional.linear(x, self.weight, self.bias))
 
 
-activation_str_to_func = {"gelu": gelu,
-                          "relu": torch.nn.functional.relu,
-                          "swish": swish}
+activation_str_to_func = {"gelu": torch.nn.functional.gelu,
+                          "relu": torch.nn.functional.relu,}
 
 torch.manual_seed(0)  # for repeatable results    
 linear_plus_gelu = LinearPlusActivation(4, 2, 'gelu')
-inp = np.array([[[[1, 2, 3, 4],  # batch(=1) x channels(=1) x height(=3) x width(=4)
-                  [1, 2, 3, 4],
-                  [1, 2, 3, 4]]]])
-x = torch.tensor(inp, dtype=torch.float)
+x = torch.tensor([[[[1, 2, 3, 4],  # batch(=1) x channels(=1) x height(=3) x width(=4)
+                    [5, 6, 7, 8],
+                    [9, 1, 2, 3]]]], dtype=torch.float32)
 print(linear_plus_gelu(x))  # fused layer
 
 # Unfused version that gives the same result
 torch.manual_seed(0)  # reset the random num generator
-linear = torch.nn.Linear(4, 2)
+linear = torch.nn.Linear(4, 2, True)
 gelu_activation = nn.GELU()
 print(gelu_activation(linear(x)))
-
-
